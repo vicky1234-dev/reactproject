f66126734b0a66d179355280917bb073c4a1afd4
vickysingh12 <vicky.bhullar1000@gmail.com>
2020-10-19 17:51:14 +0530

    Updates

---
+++ precache-manifest.545320ebb2b337e704a7f0097d2ccea5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076078b686aab3c58caed3e265c4fa94",
+    "url": "/reactproject/index.html"
+  },
+  {
+    "revision": "848aca1cc37fd90b09f3",
+    "url": "/reactproject/static/css/main.8c934bc3.chunk.css"
+  },
+  {
+    "revision": "26e9cc482a9f6f5541a0",
+    "url": "/reactproject/static/js/2.651d4779.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactproject/static/js/2.651d4779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848aca1cc37fd90b09f3",
+    "url": "/reactproject/static/js/main.3ae83d64.chunk.js"
+  },
+  {
+    "revision": "96a92e0eff99503ccbb7",
+    "url": "/reactproject/static/js/runtime-main.21b2910b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
